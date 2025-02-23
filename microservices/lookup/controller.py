c5a17c3444faadcffc2b17caba6677991b5e3790
Miles Edwards <miles.mfe@gmail.com>
2025-02-23 18:10:24 +0000

    Add D4 logic without API or Database connection

--- microservices/lookup/controller.py
+++ microservices/lookup/controller.py
@@ -1,3 +1,20 @@
 class LookupController:
-    def index(self):
-        return {'message':'Hello, World!'}
+    def convert_fragment(self, request):
+        required_fields = {'fragment'}
+        
+        # Check request
+        if not request: return {'error': 'Missing required fields'}, 400
+        if not all(field in request for field in required_fields):
+            return {'error': 'Missing required fields'}, 400
+        
+        # TODO: Check if fragment is valid
+        
+        # TODO: Audd.io API connection: Convert fragment to music
+        
+        # TODO: Database connection: Check if music exists in database
+        
+        # TODO: Database connection: Get music from database
+        full_track = {'name': 'Name', 'artist': 'Artist', 'album': 'Album', 'genre': 'Genre', 'duration': 0}
+        track_id = 0
+        
+        return {'full_track': full_track, 'track_id': track_id}, 200
--- microservices/lookup/route.py
+++ microservices/lookup/route.py
@@ -1,11 +1,15 @@
-from flask import Blueprint, make_response, jsonify
+from flask import Blueprint, make_response, jsonify, request
 from controller import LookupController
 
 
 lookup_bp = Blueprint('lookup', __name__)
 lookup_controller = LookupController()
-@lookup_bp.route('/', methods=['GET'])
-def index():
-    result=lookup_controller.index()
-    return make_response(jsonify(data=result))
+@lookup_bp.route('/', methods=['POST'])
+def convert_fragment():
+    try:
+        result, code = lookup_controller.convert_fragment(request.json)
+        return make_response(jsonify(result), code)
+    except Exception as e:
+        print(e)
+        return make_response(jsonify({'error': "Internal Server Error"}), 500)
       
