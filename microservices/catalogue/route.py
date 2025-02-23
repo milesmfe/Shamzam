1a626751fb2cbcfb5b83aafd2a34c4746ea964c8
Miles Edwards <miles.mfe@gmail.com>
2025-02-23 16:57:15 +0000

    Add logic for add_track without db connection

--- microservices/catalogue/route.py
+++ microservices/catalogue/route.py
@@ -7,5 +7,8 @@ catalogue_controller = CatalogueController()
     
 @catalogue_bp.route('/tracks/add', methods=['POST'])
 def add_track():
-  result = catalogue_controller.add_track(request.json)
-  return make_response(jsonify(result))
+  try:
+    result, code = catalogue_controller.add_track(request.json)
+    return make_response(jsonify(result), code)
+  except Exception:
+    return make_response(jsonify({'error': 'Internal Server Error'}), 500)
